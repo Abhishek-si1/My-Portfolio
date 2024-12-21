6d1cea2e31d4bb2bd1d6615aca2985c8d63ca8c4
Abhishek-si1 <abhisheksingh283420@gmail.com>
2024-12-22 04:13:19 +0530

    deleted:    file-removebg-preview-sBwKo4C-n-transformed.png 	new file:   file.png 	modified:   homepage.html 	new file:   moon.png 	modified:   script.js 	modified:   style.css 	new file:   sun.png

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("This page is UNRESPONSIVE !!!");
+const modebtn = document.querySelector("#togglebtn");
+const body = document.querySelector("body");
+
+let currentMode = 'light';
+
+modebtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        icon.src = 'moon.png';
+        btnpara.innerText = "Dark Mode";
+        body.classList.add('dark');
+        body.classList.remove('light');
+    } else {
+        icon.src = 'sun.png';
+        btnpara.innerText = "Light Mode";
+        body.classList.add('light');
+        body.classList.remove('dark');
+    }
+});
